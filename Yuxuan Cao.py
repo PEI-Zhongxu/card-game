fa203c14a0bfef98b1b6172c95d6f5c1689d9c37
Capyuxuan <u5662452@live.warwick.ac.uk>
2025-03-13 15:29:58 +0000

    Add files via upload

---
+++ Yuxuan Cao.py
@@ -0,0 +1,4 @@
+Python 3.13.0 (v3.13.0:60403a5409f, Oct  7 2024, 00:37:40) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello, yuxuan!")
+Hello, yuxuan!
